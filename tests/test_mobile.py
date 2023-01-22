9e2dfadeb9764d6b559d598248d5ff5d089a6bdd
sakhr-qa-autotest <salavat.kharasov@redlab.dev>
2023-01-22 18:15:27 +0400

    Button-label

--- tests/test_mobile.py
+++ tests/test_mobile.py
@@ -6,7 +6,7 @@ from selene.support.shared import browser
 @pytest.mark.parametrize('browser_mobile', ["540x960"], indirect=True)
 def test_github_mobile_indirect(browser_mobile):
     browser.open('https://github.com')
-    browser.element('div>[aria-label="Toggle navigation"]').click()
+    browser.element('.Button-label').click()
     browser.element('a[href="/login"]').click()
     browser.element('.auth-form-header').should(have.text("Sign in to GitHub"))
 
@@ -21,6 +21,6 @@ def test_github_mobile(browser_mobile, width, height):
     else:
         browser.open('https://github.com')
         browser.driver.set_window_size(width, height)
-        browser.element('div>[aria-label="Toggle navigation"]').click()
+        browser.element('.Button-label').click()
         browser.element('a[href="/login"]').click()
         browser.element('.auth-form-header').should(have.text("Sign in to GitHub"))
