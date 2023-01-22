65b8184361e8c0e179d9b78b110489d3451cf2e0
sakhr-qa-autotest <salavat.kharasov@redlab.dev>
2023-01-22 18:06:58 +0400

    Разрешения

--- tests/test_desktop.py
+++ tests/test_desktop.py
@@ -3,7 +3,7 @@ from selene import have
 from selene.support.shared import browser
 
 
-@pytest.mark.parametrize('browser_desktop', ["1920x1080"], indirect=True)
+@pytest.mark.parametrize('browser_desktop', ["1440x900"], indirect=True)
 def test_github_desktop_indirect(browser_desktop):
     browser.open('https://github.com')
     browser.element('a[href="/login"]').click()
@@ -11,7 +11,7 @@ def test_github_desktop_indirect(browser_desktop):
 
 
 @pytest.mark.parametrize('width, height', [
-    pytest.param(1920, 1080, id='Browser size: 1920x1080'),
+    pytest.param(1600, 1200, id='Browser size: 1600x1200'),
     pytest.param(540, 960, id='Browser size: 540x960')
 ])
 def test_github_desktop(browser_desktop, width, height):
